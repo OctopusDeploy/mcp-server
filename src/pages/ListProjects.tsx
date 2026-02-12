df315609965eeadcffa3efbaf1ea10a9e72e21ca
Yang Guo <yangguoif@gmail.com>
2026-02-13 02:30:54 +1300

    Show releases

--- src/pages/ListProjects.tsx
+++ src/pages/ListProjects.tsx
@@ -4,6 +4,8 @@ import {useCallback, useEffect, useState} from "react";
 import * as React from "react";
 import {Button, Callout, SimpleDataTable} from "@octopusdeploy/design-system-components";
 import {OctopusIcon} from "@octopusdeploy/design-system-icons";
+import {css} from "@emotion/css";
+import {space, themeTokens} from "@octopusdeploy/design-system-tokens";
 
 interface ProjectsProps {
     app: App;
@@ -12,7 +14,7 @@ interface ProjectsProps {
 }
 export function ListProjects({ app, toolResult }: ProjectsProps) {
     const [projects, setProjects] = useState<OctopusProject[]>([]);
-    const [releaseCreated, setReleaseCreated] = useState<string>("");
+    const [releasesInProject, setReleasesInProject] = useState<ReleaseInProject[]>([]);
     useEffect(() => {
         if (toolResult) {
             setProjects(extractProjects(toolResult));
@@ -29,33 +31,60 @@ export function ListProjects({ app, toolResult }: ProjectsProps) {
             //     ProjectName: project.name,
             // });
             //
-            const result = await app.callServerTool({ name: "create_release", arguments: {spaceName: "Default", projectName: project.name} });
+            const result = await app.callServerTool({ name: "list_releases_for_project", arguments: {spaceName: "Default", projectId: project.id} });
             const { text } = result.content?.find((c) => c.type === "text")!;
             const parsedResult = JSON.parse(text);
-            await app.sendLog({level: "info", data: `*** Created ${parsedResult["releaseId"]}`});
-            setReleaseCreated(text);
+            setReleasesInProject(parsedResult.items);
         } catch (e: any) {
             await app.sendLog({level: "error", data: `**** error: ${e.message}`});
             await app.sendLog({level: "error", data: "Failed to create Release"});
         }
     }, [app]);
 
-    return <main>
-        <h1><OctopusIcon size={24} /><span>Projects</span></h1>
-        {releaseCreated && (<Callout type={"success"} title={`Release ${releaseCreated} created`} /> )}
-        <SimpleDataTable columns={[
-            { title: "Project Name", render: (project) => project.name },
-            { title: "Project description", render: (project) => project.description },
-            { title: "Disabled", render: (project) => project.isDisabled ? "Yes" : "No" },
-            { title: "Actions", render: (project) => <Button importance={"secondary"} label={"Create Release"} onClick={() => onClickCreateRelease(project)} /> },
-        ]} data={projects} getRowKey={(project) => project.id} />
-    </main>
+    return (
+        <main className={pageStyles}>
+            <h1 className={headerStyles}><OctopusIcon size={24} /><span>Projects</span></h1>
+            <SimpleDataTable columns={[
+                { title: "Project Name", render: (project) => project.name },
+                { title: "Project description", render: (project) => project.description },
+                { title: "Disabled", render: (project) => project.isDisabled ? "Yes" : "No" },
+                { title: "Actions", render: (project) => <Button importance={"secondary"} label={"Show Releases"} onClick={() => onClickCreateRelease(project)} /> },
+            ]} data={projects} getRowKey={(project) => project.id} />
+            {
+                releasesInProject.length === 0
+                    ? <Callout type={"warning"} title={"No releases in project"} />
+                    : <Callout type={"information"} title={"Releases in Project"}>
+                        <ul>
+                            {
+                                releasesInProject.map(release => {
+                                    return <li key={release.id}>{release.version}</li>
+                                })
+                            }
+                        </ul>
+                    </Callout>
+            }
+        </main>
+    )
 }
 
 function extractProjects(callToolResult: CallToolResult): OctopusProject[] {
     const { text } = callToolResult.content?.find((c) => c.type === "text")!;
     return JSON.parse(text)["items"];
 }
+
+const pageStyles = css({
+    color: themeTokens.color.text.primary,
+    backgroundColor: themeTokens.color.background.primary.default,
+    display: "flex",
+    flexDirection: "column",
+    gap: space[8],
+})
+
+const headerStyles = css({
+    display: "flex",
+    alignItems: "center",
+    gap: space[12],
+})
 
 interface OctopusProject {
     spaceId: string;
@@ -67,5 +96,14 @@ interface OctopusProject {
     lifecycleId: string;
     isDisabled: string;
     repositoryUrl: string | null;
+}
+
+interface ReleaseInProject {
+    id: string;
+    version: string;
+    channelId: string;
+    projectId: string;
+    releaseNotes: string;
+    assembled: string;
 }
 
