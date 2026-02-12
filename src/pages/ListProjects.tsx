a3477abca48c0d1c9fe9f70ed3751476b147bbff
Yang Guo <yangguoif@gmail.com>
2026-02-12 21:15:45 +1300

    parse data

--- src/pages/ListProjects.tsx
+++ src/pages/ListProjects.tsx
@@ -9,25 +9,37 @@ interface ProjectsProps {
     hostContext?: PostMessageTransport;
 }
 export function ListProjects({ app, toolResult }: ProjectsProps) {
-    const [projects, setProjects] = useState<string[]>([]);
+    const [projects, setProjects] = useState<OctopusProject[]>([]);
     useEffect(() => {
         if (toolResult) {
             setProjects(extractProjects(toolResult));
         }
     }, [toolResult]);
 
-    return <main>
+    return <main style={{ backgroundColor: "#fff" }}>
         <h1>Projects</h1>
         <ul>
             {projects.map((project) => (
-                    <li key={project}>{project}</li>
+                    <li key={project.slug}>{project.name}</li>
             ))}
         </ul>
     </main>
 }
 
-function extractProjects(callToolResult: CallToolResult): string[] {
+function extractProjects(callToolResult: CallToolResult): OctopusProject[] {
     const { text } = callToolResult.content?.find((c) => c.type === "text")!;
-    return text.split(", ");
+    return JSON.parse(text)["items"];
+}
+
+interface OctopusProject {
+    spaceId: string;
+    id: string;
+    name: string;
+    description: string;
+    slug: string;
+    deploymentProcessId: string;
+    lifecycleId: string;
+    isDisabled: string;
+    repositoryUrl: string | null;
 }
 
